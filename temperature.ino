4f6b0a06cb92c368ad2e4c4aaeb3e3ad4f2baf0e
thiampo <73480469+thiampo@users.noreply.github.com>
2021-01-04 13:10:44 +0100

    Add files via upload

---
+++ temperature.ino
@@ -0,0 +1,19 @@
+#include <dht.h>
+
+
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int chk = DHT.read11(DHT11_PIN);
+ 
+  Serial.println(DHT.temperature);
+  
+  delay(5000);
+}
